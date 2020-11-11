ff671da77ada7368124d62abc9255e092e94e5fa
junggu.ji <junggu.ji.dev@gmail.com>
2020-11-11 19:56:56 +0900

    type : init
    프로젝트 생성

---
+++ src/main/java/com/junggu/automaticgenerationoftestcode/AutomaticGenerationOfTestCodeApplication.java
@@ -0,0 +1,13 @@
+package com.junggu.automaticgenerationoftestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticGenerationOfTestCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticGenerationOfTestCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junggu/automaticgenerationoftestcode/AutomaticGenerationOfTestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junggu.automaticgenerationoftestcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticGenerationOfTestCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
